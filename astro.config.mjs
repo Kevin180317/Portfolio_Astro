f2c67ee9ee081699f3b040686aa16722837f4451
¨kevin180317¨ <ortega1gv@gmail.com>
2025-07-22 10:48:38 -0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://okhuysendev.vercel.app/",
+  site: "https://okhuysen.dev/",
   integrations: [tailwind(), sitemap(), robotsTxt(), react()],
 });
