8e1e0ec9fbb0add0951a65d037eaa31b29e40f91
¨kevin180317¨ <ortega1gv@gmail.com>
2025-07-22 10:32:53 -0700

    update analy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
@@ -10,10 +9,4 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: "https://okhuysendev.vercel.app/",
   integrations: [tailwind(), sitemap(), robotsTxt(), react()],
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
